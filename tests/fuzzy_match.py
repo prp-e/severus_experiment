e04c94bf645cd32be5eb6fe1b37667b7b3e3d666
Muhammadreza Haghiri <haghiri75@gmail.com>
2022-09-05 12:21:26 +0430

    Added two code pieces

--- tests/fuzzy_match.py
+++ tests/fuzzy_match.py
@@ -1,4 +1,16 @@
 from fuzzywuzzy import fuzz
 from fuzzywuzzy import process
 
-print("Hello, world")
+commented_code = """import cv2
+camera = cv2.VideoCapture(0)
+while True:
+    # Getting input from user and show webcam's image
+"""
+
+ai_code = """import cv2
+camera = cv2.VideoCapture(0)
+while True:
+    # Getting input from user and show webcam's image
+    ret, frame = camera.read()
+    cv2.imshow('frame', frame)
+"""
